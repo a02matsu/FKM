5994b4a7ce4ac7257a3fcb6cddbb142614fcef72
kohta610 <136406670+kohta610@users.noreply.github.com>
2023-11-13 09:39:01 +0900

    比熱の理論式の試行錯誤

--- makefig_sh_K4.jl
+++ makefig_sh_K4.jl
@@ -20,8 +20,8 @@ zeta(q) = 1 / ((1 - q) * (1 - 2q) * (1 - q^2)^2 * (1 + q + 2q^2)^3)
 
 plt = plot(xlabelfontsize=14, ylabelfontsize=14, size=(1000, 700), margin=20px)
 
-for gamma_int in [400, 800, 1600, 12800, 102400, 409600, 1638400, 13107200]
-  #for gamma_int in [102400,409600,1638400]
+#for gamma_int in [400, 800, 1600, 12800, 102400, 409600, 1638400, 13107200]
+for gamma_int in [102400,409600,1638400]
 
   # CSVファイルの読み込み
   file = "./FKM_$(NAME)/$(phys)_N$(Nc)g$(gamma_int)u00.csv"
@@ -40,8 +40,10 @@ for gamma_int in [400, 800, 1600, 12800, 102400, 409600, 1638400, 13107200]
   scatter!(plt, x, y, yerror=err, label="\$\\gamma\$ = $(g)", markershape=:circle, markersize=3)
 
   C0(s) = g^2 * log(zeta(2^(-2 * s)))
+  C0n(s) = 2*g^2 * log(zeta(2^(-2*s)))
   C1(s) = 3 / 2
   plot!(1.0:0.05:9.0, C0, label="C0(\$\\gamma\$=$(g))")
+  plot!(1.0:0.05:9.0, C0n, label="(4/3)*C0(\$\\gamma\$=$(g))")
   plot!(1.0:0.05:9.0, C1, label="C1(\$ \\gamma\$=$(g))")
 
 end
