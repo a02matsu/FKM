767bbd82dc967a133df5c114deb210317da18ea3
a02matsu <s.matsu@keio.jp>
2024-01-25 11:35:01 +0900

    pmapを使ったものに変更

--- autocorr_and_jackknife.jl
+++ autocorr_and_jackknife.jl
@@ -1,75 +1,99 @@
-include("Measurement.jl")
-using .Measurement
-using Glob
-using DelimitedFiles
-using DataFrames
-using CSV
+using Distributed
+NPROCS = 18 
+addprocs(NPROCS)
+nworkers()
+@everywhere include("Measurement.jl")
+@everywhere using .Measurement
+@everywhere using Glob
+@everywhere using DelimitedFiles
+@everywhere using DataFrames
+@everywhere using CSV
 using Plots
 
-obs = "dC"
+#@everywhere begin 
+#  obs = "specificheat"
+#  N=16
+#  gamma=16384
+#
+#  bins = range(1,stop=2000,step=1)
+#  epsilon = 10^-3
+#
+#  pattern="Obs/$(obs)_N$(N)g$(gamma)q*.txt"
+#
+#  Q = []
+#  B = []
+#  autocorr = []
+#  jmean = []
+#  jerr = []
+#end
+
+obs = "specificheat"
 N=16
 gamma=16384
 
-begin 
-  bins = range(1,stop=2000,step=10)
+pattern="Obs/$(obs)_N$(N)g$(gamma)q*.txt"
+files = glob(pattern)
+
+# 測定ファイルから自己相関とjack knife dataを読み出す関数
+@everywhere function find_jack_data(file)
+  bins = range(1,stop=2000,step=1)
   epsilon = 10^-3
-
-  pattern="Obs/$(obs)_N$(N)g$(gamma)q*.txt"
-
-  files = glob(pattern)
-  Q = []
-  B = []
-  autocorr = []
-  jmean = []
-  jerr = []
-
-  for file in files 
-    data = vec(readdlm(file, Float64))
-    # ファイル名からNcを取得
-    q = read_q(file)
-    push!(Q,q) 
-    info = 1
-    for bin in bins 
-      tmp = autocorrelation(data,bin)
-      if tmp < epsilon 
-        push!(B, bin)
-        push!(autocorr, tmp)
-        jmean0, jerr0 = jackknife(data,bin)
-        push!(jmean, jmean0)
-        push!(jerr, jerr0)
-        println(q,"\t",bin,"\t",tmp,"\t",jmean0,"\t",jerr0)
-        info = 0
-        break
-      end
-    end
-    if info == 1 
-      bin = 2000
-      tmp = autocorrelation(data,bin)
-      push!(B, bin)
-      push!(autocorr, tmp)
-      jmean0, jerr0 = jackknife(data,bin)
-      push!(jmean, jmean0)
-      push!(jerr, jerr0)
-      println("!!OVER 2000!!", q,"\t",bin,"\t",tmp,"\t",jmean0,"\t",jerr0)
+  data = vec(readdlm(file, Float64))
+  q = read_q(file)
+  info = 1
+  B, autocorr, jmean, jerr = 0, 0.0, 0.0, 0.0
+  for bin in bins
+    tmp = autocorrelation(data,bin)
+    if tmp < epsilon 
+      B = bin
+      autocorr = tmp
+      jmean, jerr = jackknife(data,bin)
+      println(q,"\t",bin,"\t",tmp,"\t",jmean,"\t",jerr)
+      info = 0
+      break
     end
   end
-  # Qのソート順を取得
-  perm = sortperm(Q)
-  # 他の配列も同じ順序でソート
-  Q_sorted = Q[perm]
-  bin_sorted = B[perm]
-  autocorr_sorted = autocorr[perm]
-  jmean_sorted = jmean[perm]
-  jerr_sorted = jerr[perm]
-  
-  # データフレームの作成
-  df = DataFrame(q=Q_sorted, s=sof.(Q_sorted),  bin=bin_sorted, autocorr=autocorr_sorted, jmean=jmean_sorted, jerr=jerr_sorted)
-  
-  # CSVファイルの名前
-  csvfile = "Obs/JN$(obs)_N$(N)g$(gamma).csv"
-  # CSVファイルに保存
-  CSV.write(csvfile, df)
+  if info == 1 
+    bin = 2000
+    tmp = autocorrelation(data,bin)
+    B, autocorr = bin, tmp
+    jmean, jerr = jackknife(data,bin)
+    println("!!OVER 2000!!", q,"\t",bin,"\t",tmp,"\t",jmean,"\t",jerr)
+  end
+  return q, B, autocorr, jmean, jerr
 end
+
+###################################
+# ファイルの処理
+# pmap により、各workerでjackknifeが実行され、
+# (q,B,autocorr,jmean,jerr)のタプルが返される。
+# resultにはこのタプルの配列が格納される
+results = pmap(find_jack_data, files)
+# 結果の分解
+Q, B, autocorr, jmean, jerr = [], [], [], [], []
+for tuple in results
+    push!(Q, tuple[1])
+    push!(B, tuple[2])
+    push!(autocorr, tuple[3])
+    push!(jmean, tuple[4])
+    push!(jerr, tuple[5])
+end
+# Qのソート順を取得
+perm = sortperm(Q)
+# 他の配列も同じ順序でソート
+Q_sorted = Q[perm]
+bin_sorted = B[perm]
+autocorr_sorted = autocorr[perm]
+jmean_sorted = jmean[perm]
+jerr_sorted = jerr[perm]
+
+# データフレームの作成
+df = DataFrame(q=Q_sorted, s=sof.(Q_sorted),  bin=bin_sorted, autocorr=autocorr_sorted, jmean=jmean_sorted, jerr=jerr_sorted)
+
+# CSVファイルの名前
+csvfile = "JNData/JN$(obs)_N$(N)g$(gamma).csv"
+# CSVファイルに保存
+CSV.write(csvfile, df)
 
 
 
