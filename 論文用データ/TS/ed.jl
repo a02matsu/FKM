6fb16d68d463380c61ef8836218e4c0f76d4123e
kohta610 <136406670+kohta610@users.noreply.github.com>
2024-02-27 21:51:55 +0900

    Update ed.jl

--- 論文用データ/TS/ed.jl
+++ 論文用データ/TS/ed.jl
@@ -4,15 +4,15 @@ using Plots
 using Plots.PlotMeasures
 
 
-file = "./論文用データ/TS/Phases/phases_N16g16384q3137E-2u00.csv"
-#file = "./論文用データ/TS/Phases/phases_N16g16384q7433E-2u00.csv"
+#file = "./論文用データ/TS/Phases/phases_N16g16384q3137E-2u00.csv"
+file = "./論文用データ/TS/Phases/phases_N16g16384q7433E-2u00.csv"
 
 df = CSV.read(file, DataFrame)
 
 cycle1 = df."Cycle 1" * 16
 cycle2 = df."Cycle 2" * 16
 
-plt = plot(xlabelfontsize=14, ylabelfontsize=14, size=(500, 350), margin=20px)
+plt = plot(xlabelfontsize=14, ylabelfontsize=14, legendfontsize=12, size=(500, 350), margin=20px)
 
 xlims!(-3.14159, 3.14159)
 ylims!(0, 0.5)
@@ -20,7 +20,7 @@ xlabel!("\$\\theta\$")
 ylabel!("\$\\rho(\\theta)\$")
 
 x = range(-3.14159, 3.14159, length=100)
-bar!(plt,x,cycle1,alpha=0.5,label="\$U_1\$ (cycle of length 3)")
-bar!(plt,x,cycle2,alpha=0.5,label="\$U_2\$ (cycle of length 4)")
+bar!(plt,x,cycle1,alpha=0.5,label="\$U_1\$")
+bar!(plt,x,cycle2,alpha=0.5,label="\$U_2\$")
 
-savefig(plt, "TS-ED.pdf")
+savefig(plt, "TS-ED1.pdf")
